d633106ac10fbedd25854635a1bd680887ee800b
bonanza_gps <luisguerrero20@hotmail.com>
2025-08-30 23:25:21 -0600

    Update index.js

--- api/reservations/index.js
+++ api/reservations/index.js
@@ -1,5 +1,5 @@
 // /api/reservations/index.js
-import { query } from '../_db.js';
+import { query } from '../db.js';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
